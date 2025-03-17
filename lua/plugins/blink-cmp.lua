1bc7c9858e504c2da4032be54f093a1c95d3d197
Ifrahim Ansari <147778089+IfrahimA@users.noreply.github.com>
2025-03-17 17:42:31 -0400

    updated LSP autocomplete settings

---
+++ lua/plugins/blink-cmp.lua
@@ -0,0 +1,19 @@
+return {
+	"saghen/blink.cmp",
+	dependencies = { "rafamadriz/friendly-snippets" },
+	version = "*",
+	opts = {
+		cmdline = {
+			keymap = {
+				preset = "default",
+				["<Tab>"] = { "show", "accept" },
+			},
+			completion = { menu = { auto_show = true } },
+		},
+		appearance = {
+			use_nvim_cmp_as_default = true,
+			nerd_font_variant = "mono",
+		},
+		signature = { enabled = true },
+	},
+}
