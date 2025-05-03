8e4229dee04ba0a953af82c9dcbc37970e93bf3c
Ifrahim Ansari <147778089+IfrahimA@users.noreply.github.com>
2025-05-02 22:08:14 -0400

    updated colorscheme

--- lua/plugins/gruvbox.lua
+++ lua/plugins/gruvbox.lua
@@ -1,9 +1,1 @@
-return {
-	"sainnhe/gruvbox-material",
-	lazy = false,
-	priority = 1000,
-	config = function()
-		vim.g.gruvbox_material_enable_italic = true
-		vim.cmd.colorscheme("gruvbox-material")
-	end,
-}
+return { "shaunsingh/nord.nvim" }
