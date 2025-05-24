2131df3b34932822ee00d98263f3d40db8745cdd
Ifrahim Ansari <147778089+IfrahimA@users.noreply.github.com>
2025-05-24 03:42:50 -0400

    removed duplicate documentation and signature

--- lua/plugins/blink-cmp.lua
+++ lua/plugins/blink-cmp.lua
@@ -1,24 +1,24 @@
 return {
-	"saghen/blink.cmp",
-	dependencies = { "rafamadriz/friendly-snippets" },
-	version = "*",
-	opts = {
-		cmdline = {
-			keymap = {
-				preset = "default",
-				["<Tab>"] = { "show", "accept" },
-			},
-			completion = { menu = { auto_show = true } },
-		},
-		appearance = {
-			use_nvim_cmp_as_default = true,
-			nerd_font_variant = "mono",
-		},
-		signature = {
-			enabled = true,
-			window = {
-				show_documentation = true,
-			},
-		},
-	},
+  "saghen/blink.cmp",
+  dependencies = { "rafamadriz/friendly-snippets" },
+  version = "*",
+  opts = {
+    cmdline = {
+      keymap = {
+        preset = "default",
+        ["<Tab>"] = { "show", "accept" },
+      },
+      completion = { menu = { auto_show = true } },
+    },
+    appearance = {
+      use_nvim_cmp_as_default = true,
+      nerd_font_variant = "mono",
+    },
+    signature = {
+      enabled = true,
+      window = {
+        show_documentation = false,
+      },
+    },
+  },
 }
--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -2,9 +2,7 @@ return {
   {
     "folke/noice.nvim",
     event = "VeryLazy",
-    opts = {
-      -- add any options here
-    },
+    opts = {},
     dependencies = {
       -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
       "MunifTanjim/nui.nvim",
@@ -27,8 +25,10 @@ return {
         },
       },
     },
-    signature = {
-      enabled = false
+    lsp = {
+      signature = {
+        enabled = false
+      },
     },
   })
 }
