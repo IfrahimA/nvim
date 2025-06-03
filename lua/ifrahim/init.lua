1594a98a84f33a6e81c49bc94dc0886f74a5f046
Ifrahim Ansari <147778089+IfrahimA@users.noreply.github.com>
2025-06-03 16:19:49 -0400

    removed neotree, too much of a headache

--- lua/ifrahim/init.lua
+++ lua/ifrahim/init.lua
@@ -40,11 +40,11 @@ vim.keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights
 
 -- Setting up Split Windows
 vim.keymap.set("n", "<leader>sh", function()
-  vim.cmd("vsplit | enew")
+	vim.cmd("vsplit | enew")
 end, { desc = "Vertical Split with Empty Buffer" })
 
 vim.keymap.set("n", "<leader>sv", function()
-  vim.cmd("split | enew")
+	vim.cmd("split | enew")
 end, { desc = "Horizontal Split with Empty Buffer" })
 
 vim.keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make Splits Equal Size" })
@@ -57,10 +57,10 @@ vim.keymap.set("n", "<leader>>", ":vertical resize +5<CR>", { desc = "Increase w
 vim.keymap.set("n", "<leader><", ":vertical resize -5<CR>", { desc = "Decrease window width" })
 
 --Setting up Tabs
-vim.keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" })                     -- open new tab
-vim.keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" })              -- close current tab
-vim.keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" })                     --  go to next tab
-vim.keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" })                 --  go to previous tab
+vim.keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" }) -- open new tab
+vim.keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" }) -- close current tab
+vim.keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  go to next tab
+vim.keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
 vim.keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
 
 -- Telescope
@@ -77,7 +77,7 @@ vim.keymap.set("t", "<Esc>", [[<C-\><C-n>]], { desc = "Exit Terminal Mode" })
 --Tree
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
-vim.keymap.set("n", "<leader>e", ":Neotree<CR>", { desc = "Toggle Nvim Tree", silent = true })
+vim.keymap.set("n", "<leader>e", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle Nvim Tree", silent = true })
 vim.api.nvim_set_keymap("n", "<leader>tc", ":NvimTreeFindFile", { desc = "Nvim Tree Find File", silent = true })
 
-vim.g.neovide_input_macos_option_key_is_meta = 'only_left'
+vim.g.neovide_input_macos_option_key_is_meta = "only_left"
